bcda269173ece4b78a2e98d74547890d64a49f38
sxy <212014472@qq.com>
2025-07-06 19:20:24 +0800

    string

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,100 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t cnt = 0;
+  for(const char* p = s; *p != '\0'; p ++) {
+    cnt ++;
+  }
+  return cnt;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *d = dst;
+  while ((*d++ = *src++));
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i]; i++)
+    dst[i] = src[i];
+  for (; i < n; i++)
+    dst[i] = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *d = dst;
+  while(*d) d++;  // 找到dst结尾
+  while((*d++ = *src++)); // 复制src
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1 && *s2 && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if(n == 0) return 0;
+  while(--n && *s1 && *s2 && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char* dst = s;
+  for(size_t i = 0; i < n; i ++) {
+    dst[i] = (unsigned char) c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  char* d = dst;
+  const char* s = src;
+
+  if(d == s) {
+    return dst; 
+  }
+
+  if(s < d && d < s + n) {
+    s += n;
+    d += n;
+    while(n --) {
+      *--d = *--s;
+    }
+  }else {
+    while(n --) {
+      *d++ = *s++;
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char* dst = (char*) out;
+  const char* src = (const char*) in;
+  for(size_t i = 0; i < n; i ++) {
+    dst[i] = src[i];
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
+  for (size_t i = 0; i < n; i++) {
+    if (p1[i] != p2[i]) {
+      return (p1[i] > p2[i]) ? 1 : -1;
+    }
+  }
+  return 0;
 }
 
 #endif
