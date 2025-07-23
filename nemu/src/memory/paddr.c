f336ce138e7efe2e3725f4964ce1ddc1c31e88e2
sxy <212014472@qq.com>
2025-07-23 17:18:34 +0800

    mtrace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,6 +56,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  printf("mtrace: read memory from 0x%08x, %d bytes\n", addr, len);
+#endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -63,6 +66,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  printf("mtrace: write memory from 0x%08x, %d bytes\n", addr, len);
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
